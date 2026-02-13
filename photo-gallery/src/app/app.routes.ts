b96644fca9f193749e0448db8348f06187749558
Aurélie <naoorelie@gmail.com>
2026-02-13 16:00:41 +0100

    navbar component

--- photo-gallery/src/app/app.routes.ts
+++ photo-gallery/src/app/app.routes.ts
@@ -1,3 +1,23 @@
 import { Routes } from '@angular/router';
+import {HomeComponent} from './home/home';
+import {AboutComponent} from './about/about';
+import {CollectionComponent} from './collection/collection';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: 'about',
+    component: AboutComponent
+  },
+  {
+    path: 'collection',
+    component: CollectionComponent
+  },
+  {
+    path: '**',
+    redirectTo: '/'
+  }
+];
--- photo-gallery/src/app/app.ts
+++ photo-gallery/src/app/app.ts
@@ -1,12 +1,13 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {NavbarComponent} from './navbar/navbar';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [NavbarComponent, RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('photo-gallery');
+  title = 'Prisme';
 }
