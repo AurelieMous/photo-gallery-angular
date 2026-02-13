19b7184dc53cdc5e94f595fd05f837995e32a3f5
Aurélie <naoorelie@gmail.com>
2026-02-13 16:22:12 +0100

    update environment name

--- photo-gallery/src/app/services/pexels.service.ts
+++ photo-gallery/src/app/services/pexels.service.ts
@@ -1,13 +1,19 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
+import {environment} from '../environments/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class PexelsService {
+  private apiUrl = environment.pexelsApiUrl;
+  private apiKey = environment.pexelsApiKey;
+
   constructor(private http: HttpClient) { }
 
   getPhotos() {
-    return this.http.get()
+    return this.http.get(`${this.apiUrl}/curated`, {
+      headers: { 'Authorization': this.apiKey }
+    });
   }
 }
