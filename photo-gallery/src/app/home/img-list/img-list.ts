92780cc300b7df7ac8e73f50a64e0cd94618f7c0
Aurélie <naoorelie@gmail.com>
2026-02-13 15:36:20 +0100

    init architecture

---
+++ photo-gallery/src/app/home/img-list/img-list.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-img-list',
+  imports: [],
+  templateUrl: './img-list.html',
+  styleUrl: './img-list.css',
+})
+export class ImgList {
+
+}
