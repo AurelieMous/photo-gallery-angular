2a4b4d92e445c2f723777a143c75b06ae4026211
Aurélie <naoorelie@gmail.com>
2026-04-08 15:22:34 +0200

    update environment files

--- photo-gallery/src/app/environments/environments.exemple.ts
+++
@@ -1,6 +0,0 @@
-export const environement = {
-  production: false,
-  pexelsApiKey: 'VOTRE_API_KEY',
-  pexelsApiUrl: 'https://api.pexels.com/v1/',
-  pexelsApiUrlVideos: 'https://api.pexels.com/'
-}
